221471e1b80cb1e166fcd16abfaf8dee90ebe1d6
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-07-16 09:57:34 +0800

    added analytics for inquiries

--- initialization/analytics.py
+++ initialization/analytics.py
@@ -1,5 +1,5 @@
 from core.utils.analytics import Grid, LargeAnalyticsCardData, MediumAnalyticsCardData, SmallAnalyticsCardData
-from .helpers.analytics import get_waitlist_count, get_total_visitors, get_new_visitors, get_returning_visitors, get_total_visits
+from .helpers.analytics import get_pending_inquiries_count, get_waitlist_count, get_total_visitors, get_new_visitors, get_returning_visitors, get_total_visits, get_unread_inquiries_count
 from core.utils.registry.analytics import register_analytics, register_analytics_item
 
 ANALYTICS = [
@@ -29,7 +29,19 @@ ANALYTICS = [
                 value_function=lambda: get_total_visits(),
                 roles=["Administrator"],
                 icon="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='analytics-icon'><path d='M10 12.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z' /><path fill-rule='evenodd' d='M.664 10.59a1.651 1.651 0 0 1 0-1.186A10.004 10.004 0 0 1 10 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0 1 10 17c-4.257 0-7.893-2.66-9.336-6.41ZM14 10a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z' clip-rule='evenodd' /></svg>"
-            )
+            ),
+            SmallAnalyticsCardData(
+                title="Unread Inquiries",
+                value_function=lambda: get_unread_inquiries_count(),
+                roles=["Administrator"],
+                icon="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='analytics-icon'><path d='M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0 0 16 4H4a2 2 0 0 0-1.997 1.884ZM18 8.118l-8 4-8-4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8.118Z' /></svg>"
+            ),
+            SmallAnalyticsCardData(
+                title="Pending Inquiries",
+                value_function=lambda: get_pending_inquiries_count(),
+                roles=["Administrator"],
+                icon="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='analytics-icon'><path d='M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0 0 16 4H4a2 2 0 0 0-1.997 1.884ZM18 8.118l-8 4-8-4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8.118Z' /></svg>"
+            ),
         ],
         roles=["Administrator"],
     )
--- initialization/helpers/analytics.py
+++ initialization/helpers/analytics.py
@@ -1,5 +1,6 @@
 from extensions.landing_page.models.waitlist import Waitlist
 from extensions.landing_page.models.visitors import Visitor, Visit
+from extensions.landing_page.models.contact_us import ContactUs
 
 def get_waitlist_count():
     return Waitlist.query.count()
@@ -15,3 +16,9 @@ def get_returning_visitors():
 
 def get_total_visits():
     return Visit.query.count()
+
+def get_unread_inquiries_count():
+    return ContactUs.query.filter_by(status='new').count()
+
+def get_pending_inquiries_count():
+    return ContactUs.query.filter_by(status='pending').count()
