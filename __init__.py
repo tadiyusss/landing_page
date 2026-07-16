c13b0eeb62e0a3016f2c06ade68048bf1a2c2959
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-07-16 10:41:59 +0800

    removed the two default roles on this extension

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 from .metadata import TEMPLATE_FOLDER, STATIC_FOLDER
 from .initialization.settings import register_settings
 from .initialization.analytics import initialize_analytics
-from .initialization.roles import initialize_roles
 from .initialization.sidebar import initialize_sidebar
 
 bp = Blueprint('landing_page', __name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER, static_url_path="/static/landing_page")
@@ -15,6 +14,5 @@ def init_extension(app, db):
         db.create_all()
         register_settings()
         initialize_analytics()
-        initialize_roles()
         initialize_sidebar()
     return bp 
