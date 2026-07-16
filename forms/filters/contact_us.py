8addf878b3a4794f6b227c8d10261e6a846f58fe
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-07-16 12:03:56 +0800

    added search and filter on inquiry page

---
+++ forms/filters/contact_us.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, DateField
+from wtforms.validators import Length
+from extensions.landing_page.models.contact_us import STATUS_CHOICES
+
+class FilterContactUsForm(FlaskForm):
+    query = StringField('Search', validators=[Length(max=100)], render_kw={"class": "text-input w-auto", "placeholder": "Search by name, email, or message..."})
+    status = SelectField('Status', choices=[('', 'All')] + STATUS_CHOICES, render_kw={"class": "text-input w-auto"})
+
+    date_start = DateField('Start Date', format='%Y-%m-%d', render_kw={"class": "text-input w-auto", "placeholder": "YYYY-MM-DD"})
+    date_end = DateField('End Date', format='%Y-%m-%d', render_kw={"class": "text-input w-auto", "placeholder": "YYYY-MM-DD"})
--- routes/dashboard/inquiries.py
+++ routes/dashboard/inquiries.py
@@ -1,17 +1,38 @@
 from extensions.landing_page import bp
 from flask import render_template, request, url_for, redirect, flash
 from extensions.landing_page.models.contact_us import ContactUs
+from extensions.landing_page.forms.filters.contact_us import FilterContactUsForm
 from core.extensions import db
 
 @bp.route('/dashboard/landing-page/inquiries')
 def manage_inquiries():
+    filter_form = FilterContactUsForm(request.args)
     page = request.args.get('page', 1, type=int)
     per_page = 10
 
-    pagination = ContactUs.query.order_by(ContactUs.created_at.desc()).paginate(page=page, per_page=per_page)
+    query = ContactUs.query.order_by(ContactUs.created_at.desc())
+
+    if filter_form.query.data:
+        query = query.filter(ContactUs.first_name.contains(filter_form.query.data) | ContactUs.last_name.contains(filter_form.query.data) | ContactUs.email.contains(filter_form.query.data) | ContactUs.message.contains(filter_form.query.data))
+
+    if filter_form.status.data:
+        query = query.filter(ContactUs.status == filter_form.status.data)
+
+    if filter_form.date_start.data:
+        query = query.filter(ContactUs.created_at >= filter_form.date_start.data)
+
+    if filter_form.date_end.data:
+        query = query.filter(ContactUs.created_at <= filter_form.date_end.data)
+
+    filter_form.process()  
+
+    filters_get_values = request.args.to_dict()
+    filters_get_values.pop('page', None)
+
+    pagination = query.paginate(page=page, per_page=per_page)
     inquiries = pagination.items
 
-    return render_template('dashboard/inquiries.html', inquiries=inquiries, pagination=pagination, per_page=per_page)
+    return render_template('dashboard/inquiries.html', inquiries=inquiries, pagination=pagination, per_page=per_page, filter_form=filter_form, filters_get_values=filters_get_values)
 
 @bp.route('/dashboard/landing-page/inquiries/delete/<string:inquiry_uuid>')
 def delete_inquiry(inquiry_uuid):
