a5c616db076213e96890d27e7aa5dbef3510e5fc
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-08-08 00:26:33 +0800

    added the theme integration for dashboard page

--- forms/filters/contact_us.py
+++ forms/filters/contact_us.py
@@ -4,8 +4,8 @@ from wtforms.validators import Length
 from extensions.landing_page.models.contact_us import STATUS_CHOICES
 
 class FilterContactUsForm(FlaskForm):
-    query = StringField('Search', validators=[Length(max=100)], render_kw={"class": "text-input w-auto", "placeholder": "Search by name, email, or message..."})
-    status = SelectField('Status', choices=[('', 'All')] + STATUS_CHOICES, render_kw={"class": "text-input w-auto"})
+    query = StringField('Search', validators=[Length(max=100)], render_kw={"class": "fd-input", "placeholder": "Search by name, email, or message..."})
+    status = SelectField('Status', choices=[('', 'All')] + STATUS_CHOICES, render_kw={"class": "fd-input"})
 
-    date_start = DateField('Start Date', format='%Y-%m-%d', render_kw={"class": "text-input w-auto", "placeholder": "YYYY-MM-DD"})
-    date_end = DateField('End Date', format='%Y-%m-%d', render_kw={"class": "text-input w-auto", "placeholder": "YYYY-MM-DD"})
+    date_start = DateField('Start Date', format='%Y-%m-%d', render_kw={"class": "fd-input", "placeholder": "YYYY-MM-DD"})
+    date_end = DateField('End Date', format='%Y-%m-%d', render_kw={"class": "fd-input", "placeholder": "YYYY-MM-DD"})
